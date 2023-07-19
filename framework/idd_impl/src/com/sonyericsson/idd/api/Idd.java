69a461f142f8dbbe95922d855637e03887001485
someone5678 <nemui3353@gmail.com>
2023-07-19 16:42:07 +0900

    framework: Add dummy In Device Diagnostics frameworks
    Change-Id: I2b76fd1e3c4c2b345473cea99f0a6feb2c05d1ab

---
+++ framework/idd_impl/src/com/sonyericsson/idd/api/Idd.java
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2023 someone5678
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.sonyericsson.idd.api;
+
+import org.json.JSONObject;
+
+public final class Idd {
+    public static void addAppDataJSON(String packageName, String versionName, int versionCode, JSONObject payload) {
+    }
+}
