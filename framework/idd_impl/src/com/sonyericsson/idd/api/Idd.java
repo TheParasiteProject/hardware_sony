0760cd856f1e18f480afe79f323eb318bd786d69
Luofan Chen <clfbbn@gmail.com>
2023-11-03 13:08:31 +0800

    framework: Add dummy libraries
    So that one can install some sony apps successfully.
    
    Change-Id: Idd71e351585c4dec15d3b0190110381a04b0d432

--- framework/idd_impl/src/com/sonyericsson/idd/api/Idd.java
+++ framework/idd_impl/src/com/sonyericsson/idd/api/Idd.java
@@ -1,5 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2023 someone5678
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -9,5 +10,8 @@ import org.json.JSONObject;
 
 public final class Idd {
     public static void addAppDataJSON(String packageName, String versionName, int versionCode, JSONObject payload) {
+    }
+
+    public static void addEvent(String pkg, String name, byte[] payload) {
     }
 }
