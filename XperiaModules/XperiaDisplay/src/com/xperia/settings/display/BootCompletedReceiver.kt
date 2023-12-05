d6ed288bb7b90f14ee03b633b1969f29de778fb5
someone5678 <someone5678@users.noreply.github.com>
2023-12-05 13:46:55 +0900

    XperiaModules: Implement modular framework
    * Credits go to https://github.com/lolipuru for the modular Xperia Modules framework
      and for logic that hides the entries if the APKs aren't present.
    
    * Rename main package from Xperia Parts > Xperia Settings
    
    * Clean-up and organize
    * Import sepolicy from XiaomiParts and adapt it
    * Import KProfiles as symlink
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: saku-bruh <notwolfyy@gmail.com>
    Co-authored-by: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
    Change-Id: I55cdbb519d510a75e0f82fde5389297b8301134b

---
+++ XperiaModules/XperiaDisplay/src/com/xperia/settings/display/BootCompletedReceiver.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.xperia.settings.display
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Starting")
+        CreatorModeUtils(context).initialize()
+    }
+
+    companion object {
+        private const val TAG = "XperiaParts"
+    }
+}
