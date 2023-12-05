74f67142cf37abf1224322bf7ac22b36bfce9b55
someone5678 <someone5678@users.noreply.github.com>
2023-12-05 13:46:55 +0900

    XperiaModules: Implement modular framework
    * Credits go to https://github.com/lolipuru for the modular Xperia Modules framework
      and for logic that hides the entries if the APKs aren't present.
    
    * Rename main package from Xperia Parts > Xperia Settings
    
    * Clean-up and organize
    * Import sepolicy from XiaomiParts and adapt it
    * Import KProfiles as symlink
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: saku-bruh <notwolfyy@gmail.com>
    Co-authored-by: Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
    Change-Id: I55cdbb519d510a75e0f82fde5389297b8301134b

---
+++ XperiaModules.mk
@@ -0,0 +1,67 @@
+#
+# Copyright (C) 2023 XperiaLabs Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Flags
+TARGET_SUPPORTS_CREATOR_MODE ?= true
+TARGET_SUPPORTS_HIGH_REFRESH_RATE ?= true
+TARGET_SUPPORTS_SOUND_ENHANCEMENT ?= true
+TARGET_SUPPORTS_EUICC ?= true
+TARGET_SUPPORTS_KPROFILES ?= true
+TARGET_SUPPORTS_SONY_FRAMEWORKS ?= true
+
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)/XperiaModules
+
+# SEPolicy
+include hardware/sony/sepolicy/common/SEPolicy.mk
+
+# Main Module
+PRODUCT_PACKAGES += \
+    XperiaSettings
+
+# Submodules
+ifeq ($(TARGET_SUPPORTS_CREATOR_MODE),true)
+PRODUCT_PACKAGES += \
+    XperiaDisplay
+endif
+
+ifeq ($(TARGET_SUPPORTS_HIGH_REFRESH_RATE),true)
+PRODUCT_PACKAGES += \
+    XperiaSwitcher
+endif
+
+ifeq ($(TARGET_SUPPORTS_SOUND_ENHANCEMENT),true)
+PRODUCT_PACKAGES += \
+    XperiaAudio \
+    XperiaDolby
+endif
+
+ifeq ($(TARGET_SUPPORTS_EUICC),true)
+PRODUCT_PACKAGES += \
+    XperiaEuicc
+endif
+
+ifeq ($(TARGET_SUPPORTS_KPROFILES),true)
+PRODUCT_PACKAGES += \
+    KProfiles
+endif
+
+ifeq ($(TARGET_SUPPORTS_SONY_FRAMEWORKS),true)
+PRODUCT_PACKAGES += \
+    com.sonyericsson.idd_impl \
+    com.sony.device
+endif
