05f297295dabf88f3e6111c7c37f76d264a15002
someone5678 <someone5678@users.noreply.github.com>
2024-06-23 23:59:17 +0900

    sony: Add libidd dummy
    * To make Sony's binaries works without in device diagnostics components
    
    Change-Id: I304bc3ecd8cfda5113fc84aa5a1e7b20a34f86ce

---
+++ libidd/idd.c
@@ -0,0 +1,22 @@
+#define LOG_TAG "libidd"
+
+#include <log/log.h>
+#include <stdint.h>
+
+int idd_create() {
+    return 233;
+}
+
+int idd_add_event() {
+    return 0;
+}
+
+void idd_destroy() { }
+
+int idd_is_probe_active() {
+    return 233;
+}
+
+int idd_set_sdata() {
+  return 233;
+}
