3b046e56a9af33c43aaf3df63ec35d4b1b36ed04
someone5678 <someone5678@users.noreply.github.com>
2024-07-07 05:01:11 +0900

    sony: Drop KProfiles
    Change-Id: Ia110af2ec23a274888cd75419b34ea719808acc0

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -12,13 +12,11 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     hardware/sony/sepolicy/qti/private \
-    hardware/sony/sepolicy/qti/private/$(SEPOLICY_PLATFORM) \
-    hardware/sony/XperiaModules/KProfiles/sepolicy/private
+    hardware/sony/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/sony/sepolicy/qti/public \
-    hardware/sony/sepolicy/qti/public/$(SEPOLICY_PLATFORM) \
-    hardware/sony/XperiaModules/KProfiles/sepolicy/public
+    hardware/sony/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
 
 ifneq ($(SEPOLICY_PLATFORM), legacy-um)
 BOARD_VENDOR_SEPOLICY_DIRS += \
